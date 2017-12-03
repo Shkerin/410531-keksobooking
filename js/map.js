bfa31d7045bf85b7b62d471103c6b0b4b849656f
Vladimir Shkerin <vladshkerin@gmail.com>
2017-12-03 16:51:12 +0300

    Drawing created DOM elements

--- js/map.js
+++ js/map.js
@@ -7,7 +7,7 @@
 
   var getRandomInt = function (min, max) {
     if (!isNumeric(min) || !isNumeric(max)) {
-      throw new TypeError('Exception of computing a random number: min or max is not numeric')
+      throw new TypeError('Exception of computing a random number: min or max is not numeric');
     } else if (min > max) {
       throw new RangeError('Exception of computing a random number: min > max');
     } else if (min === max) {
@@ -20,7 +20,7 @@
 
   var getRandomValueFromArray = function (array) {
     if (!(array instanceof Array)) {
-      throw new TypeError('Exception of computing a random value: argument function is not array')
+      throw new TypeError('Exception of computing a random value: argument function is not array');
     }
 
     var idx = getRandomInt(0, array.length - 1);
@@ -32,7 +32,7 @@
     return num;
   };
 
-  var createAnnouncements = function (announMax) {
+  var createMapPins = function (announMax) {
     var numbers = [];
     for (var i = 0; i < announMax; i++) {
       numbers.push(i + 1);
@@ -68,19 +68,19 @@
       var arr = [];
 
       var rand = getRandomInt(0, features.length - 1);
-      for (var i = 0; i < rand; i++) {
+      for (i = 0; i < rand; i++) {
         arr.push(features[getRandomInt(0, features.length - 1)]);
       }
 
       return arr;
     };
 
-    var announcements = [];
-    for (var i = 0; i < announMax; i++) {
+    var mapPins = [];
+    for (i = 0; i < announMax; i++) {
       var locationX = getRandomInt(300, 900);
       var locationY = getRandomInt(100, 500);
       var rooms = getRandomInt(1, 5);
-      announcements.push({
+      mapPins.push({
         'author': {
           'avatar': getRandomAvatar()
         },
@@ -104,8 +104,80 @@
       });
     }
 
-    return announcements;
+    return mapPins;
   };
 
-  console.log(createAnnouncements(8));
+  var renderMapPins = function (mapPins) {
+    var frag = document.createDocumentFragment();
+    for (var i = 0; i < mapPins.length; i++) {
+      var locationX = mapPins[i].location.x;
+      var locationY = mapPins[i].location.y;
+      var avatar = mapPins[i].author.avatar;
+
+      var pinElem = document.createElement('div');
+      pinElem.innerHTML =
+        '<button style="left: ' + locationX + 'px; top: ' + locationY + 'px;" class="map__pin">' +
+        '<img src="' + avatar + '" width="40" height="40" draggable="false">' +
+        '</button>';
+
+      frag.appendChild(pinElem);
+    }
+    document.querySelector('.map__pins').appendChild(frag);
+  };
+
+  var renderMapCard = function (pin) {
+    var mapCardTemplate = document.querySelector('template');
+    var mapCard = mapCardTemplate.content.cloneNode(true);
+
+    mapCard.querySelector('.popup__avatar').setAttribute('src', pin.avatar);
+    mapCard.querySelector('h3').textContent = pin.offer.title;
+    mapCard.querySelector('p > small').textContent = pin.offer.address;
+    mapCard.querySelector('.popup__price').innerHTML = pin.offer.price + '&#x20bd;/ночь';
+    mapCard.querySelector('ul + p').textContent = pin.offer.description;
+    mapCard.querySelector('.popup__avatar').setAttribute('src', pin.author.avatar);
+    mapCard.querySelector('h4 + p').textContent =
+      pin.offer.rooms + ' для ' + pin.offer.guests + ' гостей';
+    mapCard.querySelector('h4 + p + p').textContent =
+      'Заезд после ' + pin.offer.checkin + ', выезд до ' + pin.offer.checkout;
+
+    // Заполнение типа жилья
+    var typeElem = mapCard.querySelector('h4');
+    switch (pin.offer.type) {
+      case 'flat':
+        typeElem.textContent = 'Квартира';
+        break;
+      case 'bungalo':
+        typeElem.textContent = 'Бунгало';
+        break;
+      case 'house':
+        typeElem.textContent = 'Дом';
+    }
+
+    // Заполнение удобств в квартире
+    var featuresElem = mapCard.querySelector('.popup__features');
+    while (featuresElem.children.length) {
+      featuresElem.removeChild(featuresElem.lastElementChild);
+    }
+    for (var i = 0; i < pin.offer.features.length; i++) {
+      var featureElem = document.createElement('li');
+      featureElem.className = 'feature feature--' + pin.offer.features[i];
+      featuresElem.appendChild(featureElem);
+    }
+
+    var mapFiltersContainer = document.querySelector('.map__filters-container');
+    document.querySelector('.map').insertBefore(mapCard, mapFiltersContainer);
+  };
+
+  var removeClass = function (selector, className) {
+    var el = document.querySelector(selector);
+    if (el !== null) {
+      el.classList.remove(className);
+    }
+  };
+
+  removeClass('.map', 'map--faded');
+
+  var mapPins = createMapPins(8);
+  renderMapPins(mapPins);
+  renderMapCard(mapPins[0]);
 })();
