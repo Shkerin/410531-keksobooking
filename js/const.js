e7780e8b852574ef5f5f1f41ec544d54d5443086
Vladimir Shkerin <vladshkerin@gmail.com>
2017-12-22 14:11:30 +0300

    Create module "consts.js"

---
+++ js/const.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+  window.consts = {
+    ESC_KEYCODE: 27,
+    ENTER_KEYCODE: 13,
+    URL_LOAD: 'https://js.dump.academy/keksobooking/data',
+    URL_UPLOAD: 'https://js.dump.academy/keksobooking',
+    TIMEOUT: 3000,
+    RESPONSE_TYPE: 'json'
+  };
+})();
