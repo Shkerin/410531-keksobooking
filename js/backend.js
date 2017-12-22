6f1f4deb7f495df5c9a0654bca3fb909d28da8b9
Vladimir Shkerin <vladshkerin@gmail.com>
2017-12-22 14:11:49 +0300

    Create module "backend.js"

---
+++ js/backend.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Модуль для взаимодействия с удалённым севером через XHR
+ */
+
+(function () {
+  var consts = window.consts;
+
+  var load = function (loadHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = consts.RESPONSE_TYPE;
+    xhr.timeout = consts.TIMEOUT;
+
+    xhr.addEventListener('load', function () {
+      loadHandler(xhr.response);
+    });
+    xhr.addEventListener('error', function () {
+      errorHandler('Произошла ошибка соединения')
+    });
+    xhr.addEventListener('timeout', function () {
+      errorHandler('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', consts.URL_LOAD);
+    xhr.send();
+  };
+
+  var upload = function (data, loadHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = consts.RESPONSE_TYPE;
+
+    xhr.addEventListener('load', loadHandler);
+    xhr.addEventListener('error', errorHandler);
+
+    xhr.open('POST', consts.URL_UPLOAD);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    upload: upload
+  }
+})();
